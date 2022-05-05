5b5b183a416dca84bce985c5ae4e7902935a8a70
Danil Stepanov <mathwaxid@gmail.com>
2022-05-06 01:02:01 +0700

    keep learning

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from webapp.models import db, News
 
 
 def get_html(url):
@@ -16,18 +18,24 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts menu').findAll('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
-            published = news.find('time').text
-            result_news.append({
-                'title': title, 
-                'url': url,
-                'published': published,
-            })
-        return result_news
-    return False
+            published = news.find('time')['datetime']
+            try:
+                published = datetime.strptime(published, '%Y %m %d')
+            except(ValueError, TypeError):
+                published = datetime.now()
+            save_news(title, url, published)
+
+
+def save_news(title, url, published):
+    news_exist = News.query.filter(News.url == url).count()
+    if not news_exist:
+        news = News(title=title, url=url, published=published)
+        db.session.add(news)
+        db.session.commit()
+
 
 if __name__ == '__main__':
-    get_python_news()
+    print('jopa')
